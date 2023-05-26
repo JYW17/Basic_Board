9a1bf2fd1cb65f79de3f5655c0b0a74c496fb6c5
JYW17 <cutr1005@naver.com>
2023-05-26 20:23:42 +0900

    feat Paging Process, Searching by Keyword

--- src/main/java/com/finalproject/board/controller/BoardController.java
+++ src/main/java/com/finalproject/board/controller/BoardController.java
@@ -3,12 +3,17 @@ package com.finalproject.board.controller;
 import com.finalproject.board.entity.Board;
 import com.finalproject.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -19,9 +24,29 @@ public class BoardController {
 
     // 게시글 목록
     @GetMapping("/board/list") //localhost:8080/board/list
-    public String boardList(Model model){
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC)
+                            Pageable pageable,
+                            String searchKeyword)
+    {
 
-        model.addAttribute("list", boardService.boardList());
+        Page<Board> list = null;
+
+        if (searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        } else{
+            list = boardService.boardSearchList(searchKeyword, pageable);
+        }
+
+        int nowPage = list.getPageable().getPageNumber() + 1; // 0부터 시작하므로 1 추가
+        int startPage = Math.max(nowPage - 4, 1); // 음수 방지
+        int endPage = Math.min(nowPage + 5, list.getTotalPages()); // 최대 페이지 초과 방지
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
 
         return "boardList";
     }
@@ -35,12 +60,12 @@ public class BoardController {
 
     // 게시글 작성 처리
     @PostMapping("board/writePro") //localhost:8080/board/writePro
-    public String boardWritePro(Board board, Model model){
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
 
-        boardService.boardWrite(board);
+        boardService.boardWrite(board, file);
 
         model.addAttribute("message", "게시글이 등록되었습니다.");
-        model.addAttribute("searchUrl", "/board/list");
+        model.addAttribute("nextPageUrl", "/board/list");
 
         return "message";
     }
@@ -72,13 +97,13 @@ public class BoardController {
 
     // 게시글 수정 처리
     @PostMapping("/board/update/{id}") //localhost:8080/board/update
-    public String boardUpdate(@PathVariable("id") Integer id, Board boardModified){
+    public String boardUpdate(@PathVariable("id") Integer id, Board boardModified, MultipartFile file) throws Exception{
 
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(boardModified.getTitle());
         boardTemp.setContent(boardModified.getContent());
 
-        boardService.boardWrite(boardTemp);
+        boardService.boardWrite(boardTemp, file);
 
         return "redirect:/board/list";
     }
